29cd5e99a70decbc6b6ae3537cfe7ed4725f954f
pei1001 <95124788+pei1001@users.noreply.github.com>
2024-06-24 08:17:53 +0200

    Corregido error duplicated code  #1

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -31,46 +31,55 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration<Rental> rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = rentals.nextElement();
-			// determine amounts for each line
-			thisAmount = each.getCharge();			
-			frequentRenterPoints += each.getFrecuentRenterPoints();
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
-		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
-		return result;
-	}
-	
-	 public String htmlStatement() {
-		 double totalAmount = 0;
-		 int frequentRenterPoints = 0;
-		 Enumeration<Rental> rentals = _rentals.elements();
-		 String result = "<H1>Rental Record for " + getName() + "</H1>";
-		 while (rentals.hasMoreElements()) {
-			 double thisAmount = 0;
-			 Rental each = rentals.nextElement();
-			 // determine amounts for each line
-			 thisAmount = each.getCharge();
-			 frequentRenterPoints += each.getFrecuentRenterPoints();
-			 // show figures for this rental
-			 result += "<H2>" + each.getMovie().getTitle() + " " + String.valueOf(thisAmount) + "</H2>";
-			 totalAmount += thisAmount;
-		 }
-		 // add footer lines
-		 result += "<P>Amount owed is " + String.valueOf(totalAmount) + "</P>";
-		 result += "<P> You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points </P>";
-		 return result;
-	 }
+        return generateStatement("text");
+    }
+
+    public String htmlStatement() {
+    	System.out.println(generateStatement("html"));
+        return generateStatement("html");
+    }
+
+    private String generateStatement(String format) {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = formatHeader(format);
+        
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            double thisAmount = each.getCharge();
+            frequentRenterPoints += each.getFrecuentRenterPoints();
+            result += formatRentalLine(format, each, thisAmount);
+            totalAmount += thisAmount;
+        }
+
+        result += formatFooter(format, totalAmount, frequentRenterPoints);
+        return result;
+    }
+
+    private String formatHeader(String format) {
+        if (format.equals("html")) {
+            return "<H1>Rental Record for " + getName() + "</H1>";
+        } else {
+            return "Rental Record for " + getName() + "\n";
+        }
+    }
+
+    private String formatRentalLine(String format, Rental rental, double amount) {
+        if (format.equals("html")) {
+            return "<H2>" + rental.getMovie().getTitle() + " " + amount + "</H2>";
+        } else {
+            return "\t" + rental.getMovie().getTitle() + "\t" + amount + "\n";
+        }
+    }
+
+    private String formatFooter(String format, double totalAmount, int frequentRenterPoints) {
+        if (format.equals("html")) {
+            return "<P>Amount owed is " + totalAmount + "</P>"
+                 + "<P> You earned " + frequentRenterPoints + " frequent renter points </P>";
+        } else {
+            return "Amount owed is " + totalAmount + "\n"
+                 + "You earned " + frequentRenterPoints + " frequent renter points";
+        }
+    }
 }
